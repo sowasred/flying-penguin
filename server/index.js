7f63a33fe4313c3ca698fe6000692407cea8d39d
sowasred <muldurozan@gmail.com>
2019-07-24 20:55:08 -0400

    heroku deployment

--- server/index.js
+++ server/index.js
@@ -1,23 +1,31 @@
-import express from 'express';
-import cors from 'cors';
-import monk from 'monk';
-import { Filter } from 'bad-words'
-const app = express();
+const express = require('express');
+const cors = require('cors');
+const Filter = require('bad-words');
 
+
+const app = express();
+app.use(cors());
 // const db = monk()
 
-// const db = monk(process.env.MONGO_URI || 'mongodb+srv://ozan:<password>@cluster0-gnvcb.mongodb.net/test?retryWrites=true&w=majority')
 
-const db = monk('mongodb+srv://ozan:<password>@cluster0-gnvcb.mongodb.net/test?retryWrites=true&w=majority')
+
+// Retrieve
+
+// Connect to the db
 
 // import collection to const
-const wishes = db.get('wishes');
+
+
+
 const filter = new Filter()
 
 port = process.env.PORT || 3000;
 
-app.use(cors());
 
+const url = 'mongodb+srv://ozan:ozan@cluster0-gnvcb.mongodb.net/test?retryWrites=true&w=majority'; // Connection URL
+const db = require('monk')(url);
+
+const wishes = db.get('wihes')
 // Any in coming request has a content type of app json will be parse and by this middleware and put on the body
 app.use(express.json());
 
@@ -30,15 +38,15 @@ app.get('/', (req,res) => {
 app.get('/wishes', (req, res) => {
     wishes
     .find()
-    .then(wishes => {
+    .then(wishes => {        
         res.json(wishes);
     })
 })
 
 
 function isValidWish(wishData){
-    return wishData.name && wishData.name.toString().trim() !== '' &&
-    wishData.content && wishData.content.toString().trim() !== ''
+ return wishData.name && wishData.name.toString().trim() !== '' &&
+ wishData.content && wishData.content.toString().trim() !== '';
 }
 
 app.post('/wishes', (req,res) => {
